b75523a6c05ac6f8c6eae0fa1cc937f6d0f3ba9c
Madeon <k4733294@gmail.com>
2019-05-31 00:12:58 +0800

    Update 3k4733294.c
    mention modified
    char* newstr = (char *)calloc(strsize+1, sizeof(char)); 
    
    Details 
    Runtime: 0 ms, faster than 100.00% of C online submissions for To Lower Case.
    Memory Usage: 6.9 MB, less than 5.06% of C online submissions for To Lower Case.
    Next challenges:
    ---------------------------
    
    if we  plus 1 at  line 4 to 
    int strsize = strlen(str) +1;
    not at calloc 
    
    the performance is 
    8 / 8 test cases passed.
    Status: Accepted
    Runtime: 4 ms
    Memory Usage: 6.8 MB
    Submitted: 10 minutes ago

--- 709_To Lower Case/3k4733294.c
+++ 709_To Lower Case/3k4733294.c
@@ -1,30 +1,18 @@
 
+
 char * toLowerCase(char * str){
     int strsize = strlen(str) ;  //prevent memory loop leak (why have 2 size?)
-    //printf ("The charstr is %s.\n",str);
-    //printf ("The charsizeIs %d",strsize);
-    //char toLCstr[strsize];
-    //char* newstr;
-    //char* newstr = malloc(strsize*sizeof(char)); 
-    char* newstr = (char *)calloc(strsize, sizeof(char));  //must out put by char pointer so only create the malloc with char pointer not array
-    //printf ("The newstr is %s.\n",newstr);
-    //strcpy(newstr,str);
+    char* newstr = (char *)calloc(strsize+1, sizeof(char));  //must out put by char pointer so only create the malloc with char pointer not array
+                                                             //strsize  must plus 1 make iteration to last 2nd char at malloc
     for(int i=0; i < strsize; i++){  //loop
         int strcharnum = str[i]; //char position to ASCII number mapping
-        //char c = strcharnum; 
-        printf ("The i is %d.\n",i);
-        //printf ("The strcharnum is %d.\n",str[i]);
-        //printf ("The newstr is %d.\n",newstr[i]);
-        //printf ("The strchar is %c.\n",c);
+ 
         if (strcharnum < 91 && strcharnum > 64){   //if char is maping on uppercase ascii
-            strcharnum = strcharnum + 32; //translate to lowercase (mapping with ascii)
-             //printf ("The strcharnum  to lower is %d.\n",strcharnum);
-            
+            strcharnum = strcharnum + 32; //translate to lowercase (mapping with ascii)            
         }
-        //char a = strcharnum;
         newstr[i] = strcharnum ;  //store to newstr with position result
-        //printf ("toLCstr %d.\n",*(newstr+i));
     }
    return newstr;
 }
+
 
