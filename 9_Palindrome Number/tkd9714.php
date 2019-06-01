ba60e9250958bd52e5561bc6bea778228757de2f
tkd9714 <49507479+tkd9714@users.noreply.github.com>
2019-06-01 21:47:11 +0800

    Update tkd9714.php

--- 9_Palindrome Number/tkd9714.php
+++ 9_Palindrome Number/tkd9714.php
@@ -5,29 +5,18 @@ class Solution {
      * @return Boolean
      */
     function isPalindrome($x) {
+        if ($x < 0)
+            return false;
+        else;
+        
         #數字轉字串
         $strnum = strval($x);
         #字串反轉
         $revers = strrev($strnum);
-        $len = strlen($x);
-        $odd = $len % 2;
-        #取中間點
-        $mid = ($len / 2);
-        if(1 == $odd)
-        {
-            $mid += 1;
-        }
-        else;
         
-        #比對
-        for($i = 0; $i < $mid; $i++)
-        {
-            if ($strnum[$i] != $revers[$i])
-            {
-                return false;
-            }
-        }
-        
-        return true;
+        if ($strnum === $revers)
+            return true;
+        else
+            return false;
     }
 }
