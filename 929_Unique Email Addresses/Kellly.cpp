d496f9dae294d59e478f246017a924b0122a23f9
housemeow <housemeow@gmail.com>
2019-06-03 15:35:56 +0800

    Update Kellly.cpp

--- 929_Unique Email Addresses/Kellly.cpp
+++ 929_Unique Email Addresses/Kellly.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int numUniqueEmails(vector<string>& emails) {
-        map<string, set<string>> maps;
+        set<string> sets;
         
         for(int i = 0; i < emails.size(); i++) {
             string email;
@@ -9,30 +9,17 @@ public:
             for (int j = 0; j < emails[i].size(); j++) {
                 if (emails[i][j] == '+') {
                     stop = true;
-                } else if (emails[i][j] == '.') {
                 } else if (emails[i][j] == '@') {
-                    string domain = emails[i].substr(j+1, emails[i].size());
-                    if (maps.find(domain) != maps.end()) {
-                        maps[domain].insert(email);
-                    } else {
-                        set<string> s;
-                        s.insert(email);
-                        maps[domain] = s;
-                    }
+                    string domain = emails[i].substr(j, emails[i].size());
+                    sets.insert(email + domain);
                     break;
-                } else {
+                } else if (emails[i][j] != '.') {
                     if (!stop) {
                         email += emails[i][j];   
                     }
                 }
             }
         }
-        
-        int size = 0;
-        for (map<string, set<string>>::iterator it = maps.begin(); it != maps.end(); it++) {
-            size += it->second.size();
-        }
-        
-        return size;
+        return sets.size();
     }
 };
