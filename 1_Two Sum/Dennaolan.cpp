5d7759a19f855db0029d0b7804247b22eb098fdb
Dennaolan <37359055+Dennaolan@users.noreply.github.com>
2019-06-05 10:09:48 +0900

    Update Dennaolan.cpp

--- 1_Two Sum/Dennaolan.cpp
+++ 1_Two Sum/Dennaolan.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans;
-        
-        
+
         for ( int i = 0 ; i < nums.size() ; i++ ) {
           for ( int j = i + 1 ; j < nums.size() ; j++ ) {
             if ( nums.at(i) + nums.at(j) == target ) {
