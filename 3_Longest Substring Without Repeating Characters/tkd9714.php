27e12f59ea181b7735fd9d1a25341dbb4ad0e078
tkd9714 <49507479+tkd9714@users.noreply.github.com>
2019-06-05 15:21:54 +0800

    Update tkd9714.php

--- 3_Longest Substring Without Repeating Characters/tkd9714.php
+++ 3_Longest Substring Without Repeating Characters/tkd9714.php
@@ -11,7 +11,8 @@ class Solution {
         for($i = 0; $i < strlen($s); $i++)
         {
             if (false !== strpos($str,$s[$i]))
-            {
+            {   
+                #取得除了重複字元開頭的子字串
                 $str = substr($str,strpos($str,$s[$i])+1);
             }
             
