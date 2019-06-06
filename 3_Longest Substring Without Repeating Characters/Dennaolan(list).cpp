c3f5242898fe6725bdc3ffde1d998b64730b58f2
Dennaolan <37359055+Dennaolan@users.noreply.github.com>
2019-06-06 11:54:01 +0900

    Create Dennaolan(list).cpp

---
+++ 3_Longest Substring Without Repeating Characters/Dennaolan(list).cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int ans = 0;
+        int temp = 0;
+        list<char> repeatList;
+        
+        for ( int i = 0 ; i < s.size() ; i++ ) {
+            
+          if ( isExist( repeatList, s.at(i) ) ) {
+            for ( ; !repeatList.empty() && repeatList.front() != s.at(i) ; repeatList.pop_front() );
+            repeatList.pop_front();
+          } // if exist, end counting and reset
+            
+          repeatList.push_back( s.at(i) );
+          ans = ans > repeatList.size() ? ans : repeatList.size()  ;
+        }
+        
+        return ans;
+    }
+
+private:
+    bool isExist( list<char> checkList, char key ) {
+      list<char>::iterator it;
+      for( it = checkList.begin() ; it != checkList.end() ; it++ ) {
+        if ( *it == key )
+          return true;
+      }
+
+      return false;
+    } // bool
+    
+};
