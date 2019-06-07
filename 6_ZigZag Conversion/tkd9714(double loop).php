ca5cd9314585e1cc535f1b9e3e0b460d5bb2cba8
tkd9714 <49507479+tkd9714@users.noreply.github.com>
2019-06-07 22:45:57 +0800

    Create tkd9714(double loop).php

---
+++ 6_ZigZag Conversion/tkd9714(double loop).php
@@ -0,0 +1,41 @@
+class Solution {
+
+    /**
+     * @param String $s
+     * @param Integer $numRows
+     * @return String
+     */
+    function convert($s, $numRows) {
+        if (strlen($s) < 1)
+            return "";
+        else;
+        
+        $ans = array_fill(0, $numRows, '');
+        
+        $j = 0;
+        for($i = 0; $i < strlen($s); $i++)
+        {
+            $ans[$j] .= $s[$i];
+
+            if ($j < $numRows-1)
+                $j++;
+            else
+            {
+                $j--;
+                while($j > 0)
+                {
+                    $i++;
+                    $ans[$j] .= $s[$i];
+                    $j--;
+                }
+                
+                if ($j < 0)
+                    $j = 0;
+                else;
+            }
+            
+        }
+        
+        return implode($ans);
+    }
+}
