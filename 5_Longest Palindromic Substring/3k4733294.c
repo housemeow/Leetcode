31faec27a39b12df63e53ca125db08b913112c4a
Madeon <k4733294@gmail.com>
2019-06-10 23:54:31 +0800

    Update 3k4733294.c

--- 5_Longest Palindromic Substring/3k4733294.c
+++ 5_Longest Palindromic Substring/3k4733294.c
@@ -1,6 +1,8 @@
 //looping every result O(n^2)
 //store the longest result to the char array
-
+//set looping size add and keep forward like
+//abbc   a  b  b  c  next loop  ab bb bc next abb bbc  abbc
+//point: if find any maximum result in the window size then jump to next window size plus 1
 char * longestPalindrome(char * s){
 
 }
