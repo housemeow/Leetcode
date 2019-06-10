a3a0a5cff947ed7c9d1914e04c459a72ca778697
Madeon <k4733294@gmail.com>
2019-06-10 23:54:57 +0800

    Update 3k4733294.c
    //looping every result O(n^2)
    //store the longest result to the char array
    //set looping size add and keep forward like
    //abbc   a  b  b  c  next loop  ab bb bc next abb bbc  abbc
    //point: if find any maximum result in the window size then jump to next window size plus 1

--- 5_Longest Palindromic Substring/3k4733294.c
+++ 5_Longest Palindromic Substring/3k4733294.c
@@ -1,4 +1,4 @@
-//looping every result O(n^2)
+///looping every result O(n^2)
 //store the longest result to the char array
 //set looping size add and keep forward like
 //abbc   a  b  b  c  next loop  ab bb bc next abb bbc  abbc
