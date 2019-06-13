70037fee06100a47a9421fb1d666932a66999ef0
housemeow <housemeow@gmail.com>
2019-06-13 15:31:55 +0800

    Update CopyPerformance.js

--- _Scripts/CopyPerformance.js
+++ _Scripts/CopyPerformance.js
@@ -1,4 +1,4 @@
-var match = document.querySelector('.container__nthg').innerText.split('\n').join('').match(/(\d+ ms).+(\d+\.\d+%).+(\d+ MB).+(\d+\.\d+%)/)
+var match = document.querySelector('.container__nthg').innerText.split('\n').join('').match(/(\d+ ms)[^\d]+(\d+\.\d+%).+(\d+ MB)[^\d]+(\d+\.\d+%)/)
 var child = document.createElement('input');
 child.id = 'test'
 child.value = `${match[1]} **(${match[2]})**, ${match[3]} **(${match[4]})**`;
