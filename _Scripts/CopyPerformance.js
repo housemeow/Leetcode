b31c2cbab6501340c8f29cb032264485194931e3
housemeow <housemeow@gmail.com>
2019-06-13 15:28:54 +0800

    Create CopyPerformance.js

---
+++ _Scripts/CopyPerformance.js
@@ -0,0 +1,7 @@
+var match = document.querySelector('.container__nthg').innerText.split('\n').join('').match(/(\d+ ms).+(\d+\.\d+%).+(\d+ MB).+(\d+\.\d+%)/)
+var child = document.createElement('input');
+child.id = 'test'
+child.value = `${match[1]} **(${match[2]})**, ${match[3]} **(${match[4]})**`;
+document.documentElement.appendChild(child);
+child.select()
+document.execCommand("copy");
