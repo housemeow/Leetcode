4eb60bc2025ca7dfde96b59a0061716ffd00ca8b
Madeon <k4733294@gmail.com>
2019-06-18 19:56:19 +0800

    Update 3k4733294.c
    Line 17: Char 10: runtime error: member access within null pointer of type 'struct ListNode' (solution.c)
    first commit

--- 21_Merge Two Sorted Lists/3k4733294.c
+++ 21_Merge Two Sorted Lists/3k4733294.c
@@ -1,1 +1,27 @@
-commit here
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+// do not store array and position
+// compare every link list node max or min and make new link then itereation to next
+//Line 14: Char 5: runtime error: member access within null pointer of type 'struct ListNode' (solution.c)
+//create statement to eliminate the link list pointer is not null
+
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
+    //struct ListNode* current1 = &l1;
+    //printf("l1 value is %d",current1->val);
+    if(l1!=NULL&&l2!=NULL&&l1->next!=NULL&&l2->next!=NULL){ }
+         printf("l1.val value is %d",l1->val);
+        l1=l1->next;
+        printf("l1.val next value is %d",l1->val);
+        l1=l1->next;
+        printf("l1.val next next value is %d",l1->val);
+   
+   
+    //struct ListNode* current2 = &l2;
+    //printf("\nl2 value is %d",current2->val);
+    return l1;
+}
