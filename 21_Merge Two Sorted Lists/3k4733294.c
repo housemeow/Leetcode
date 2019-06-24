671d37abaff1d9ab7af43058361e3eb71111ea22
Madeon <k4733294@gmail.com>
2019-06-24 19:54:41 +0800

    Update 3k4733294.c
    fail  not correct of link list concept

--- 21_Merge Two Sorted Lists/3k4733294.c
+++ 21_Merge Two Sorted Lists/3k4733294.c
@@ -19,42 +19,49 @@
 // l2  1->2
 
 struct LstNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
-    struct ListNode *merge = NULL;
+    struct ListNode *temp_node = NULL;
     struct ListNode *temp_merge = NULL;
-    //printf("l1 value is %d",current1->val);
-    //if(l1!=NULL&&l2!=NULL&&l1->next!=NULL&&l2->next!=NULL){ }
-        //printf("l1.val value is %d",l1->val);
-        //printf("56 l1 is %d",l1);
-        //l1=l1->next;
-        //printf("l1.val next value is %d",l1->val);
-        //printf("56 l1 is %d",l1);
-        //l1=l1->next;
-        //printf("l1.val next next value is %d",l1->val);
-        //printf("56 l1 is %d",l1);
+    
     bool first = true;
     while(l1->next!=NULL||l2->next!=NULL){
-        if(first == false){  //do add every number link next and iterate to next
+         if(first == false){  //do add every number link next and iterate to next
             if(l1->val<=l2->val){
-                temp_merge->next = l1;
+                temp_node = l1;
+                temp_merge->next = temp_node;
                 temp_merge = temp_merge->next;
-                l1=l1->next;
+                temp_node = l2;
+                temp_merge->next = temp_node;
+                temp_merge = temp_merge->next;
+                printf("33 temp_merge is %d",temp_merge);
+                l2 = l2->next;
+                l1 = l1->next;
                 printf("38 l1.val value is %d",l1->val);
                 printf("39 l1 is %d",l1);
+                printf("40 temp_merge is %d",temp_merge);
             }else if(l1->val>l2->val){
-                temp_merge->next = l2;
+                temp_node = l2;
+                temp_merge->next = temp_node;
                 temp_merge = temp_merge->next;
-                l2=l2->next;
+                temp_node = l1;
+                temp_merge->next = temp_node;
+                temp_merge = temp_merge->next;
+                printf("41 temp_merge is %d",temp_merge);
+                l2 = l2->next;
+                l1 = l1->next;
                 printf("42 l2.val value is %d",l2->val);
+                printf("43 l2 is %d",l2);
+                printf("44 temp_merge is %d",temp_merge);
             }
         }
-        if(first ==true){
+        if(first == true){
             if(l1->val<=l2->val){  //first head of which list //and link first
-                merge = l1;
-                printf("\n49 merge is %d",merge);
                 temp_merge = l1;
+                printf("\n49 merge is %d",merge);
+                //temp_merge = l1;
                 printf("\n50 temp_merge->val value is %d",temp_merge->val);
                 printf("\n50 temp_merge is %d",temp_merge);
-                temp_merge->next = l2;
+                temp_node = l2;
+                temp_merge->next = temp_node;
                 printf("\n52 temp_merge->val value is %d",temp_merge->val);
                 printf("\n52 temp_merge is %d",temp_merge);
                 temp_merge = temp_merge->next;
@@ -63,12 +70,21 @@ struct LstNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
                 printf("\n56 l1 is %d",l1);
                 printf("\n56 l2 is %d",l2);
             }else if(l1->val>l2->val){
-                merge = l2;
                 temp_merge = l2;
-                temp_merge->next = l1;
+                printf("\n69 merge is %d",merge);
+                //temp_merge = l1;
+                printf("\n70 temp_merge->val value is %d",temp_merge->val);
+                printf("\n72 temp_merge is %d",temp_merge);
+                temp_node = l1;
+                temp_merge->next = temp_node;
+                printf("\n75 temp_merge->val value is %d",temp_merge->val);
+                printf("\n76 temp_merge is %d",temp_merge);
                 temp_merge = temp_merge->next;
-                printf("\n59 l1.val value is %d",l1->val);
-                printf("\n60 l2.val value is %d",l2->val);
+                printf("\n78 l1.val value is %d",l1->val);
+                printf("\n79 l2.val value is %d",l2->val);
+                printf("\n80 l1 is %d",l1);
+                printf("\n81 l2 is %d",l2);
+                
             }
             first = false;
             l2=l2->next;
@@ -85,3 +101,4 @@ struct LstNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
     //printf("\nl2 value is %d",current2->val);
     return merge;
 }
+
