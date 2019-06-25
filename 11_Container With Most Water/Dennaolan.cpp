13013c4ddc5f48229a6c2eb55f87f364d2230de4
Dennaolan <37359055+Dennaolan@users.noreply.github.com>
2019-06-25 09:36:11 +0900

    Update Dennaolan.cpp

--- 11_Container With Most Water/Dennaolan.cpp
+++ 11_Container With Most Water/Dennaolan.cpp
@@ -6,9 +6,8 @@ public:
           return height[0] < height[1] ? height[0] : height[1];
         
         for ( int i = 0 ; i < height.size() ; i++ ) {
-          for ( int j = i+1 ; j < height.size() ; j++ ) {
+          for ( int j = i+1 ; j < height.size() ; j++ )
             volume = ( (j-i) * ( height[i] < height[j] ? height[i] : height[j] ) ) > volume ? ( (j-i) * ( height[i] < height[j] ? height[i] : height[j] ) ) : volume;
-          }
         }
         
         return volume;
