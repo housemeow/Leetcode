1ae40745c05d90567955d01d57a9028440c41c73
housemeow <housemeow@gmail.com>
2019-07-09 15:40:49 +0800

    Update CopyPerformance.js

--- _Scripts/CopyPerformance.js
+++ _Scripts/CopyPerformance.js
@@ -1,7 +1,8 @@
-var match = document.querySelector('.container__nthg').innerText.split('\n').join('').match(/(\d+ ms)[^\d]+(\d+\.\d+%).+(\d+ MB)[^\d]+(\d+\.\d+%)/)
+var match = document.querySelector('.container__nthg').innerText.split('\n').join('').match(/Runtime:\s+(\d+\s+ms).+than\s+(.+%).+Memory Usage:\s+(.+ MB).+than\s+(.+%)/)
 var child = document.createElement('input');
 child.id = 'test'
 child.value = `${match[1]} (**${match[2]}**), ${match[3]} (**${match[4]}**)`;
+console.log(child.value)
 document.documentElement.appendChild(child);
 child.select()
 document.execCommand("copy");
