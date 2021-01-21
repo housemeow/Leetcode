680a307cd9a4b4369ddb6c7f8162c80023cab240
Agby <totoro255032@gmail.com>
2021-01-21 21:28:15 +0800

    Create Agby-2021-1-21-intuition.js
    refer 20

---
+++ 1249_Minimum Remove to Make Valid Parentheses/Agby-2021-1-21-intuition.js
@@ -0,0 +1,43 @@
+Runtime: 112 ms, faster than 47.26% of JavaScript online submissions for Minimum Remove to Make Valid Parentheses.
+Memory Usage: 48.6 MB, less than 34.81% of JavaScript online submissions for Minimum Remove to Make Valid Parentheses.
+
+var minRemoveToMakeValid = function(s) {
+    const matchMap = new Map()
+    matchMap.set('(', ')')
+    matchMap.set('{', '}')
+    matchMap.set('[', ']')
+    
+    const parenthesesArr = ['(',')','[',']','{','}']
+    const isParentheses = (letter) => parenthesesArr.includes(letter)
+    const strArr = Array.from(s)
+    const stackPool = []
+    
+    for (let index=0; index<strArr.length; index++) {
+      const letter = strArr[index]
+
+      if (isParentheses(letter)) {
+        if (stackPool.length === 0 ) {
+          stackPool.push([letter, index]) 
+        } else {
+          const match = matchMap.get(stackPool[stackPool.length - 1][0]) === letter
+          match ? stackPool.pop() : stackPool.push([letter, index])
+        }
+      }
+    }
+    
+    if (stackPool.length === 0) return s
+    
+    const filterIndex = new Map()
+    stackPool.forEach(val => filterIndex.set(val[1], true))
+  
+    let res = ''
+    
+    strArr.forEach((letter, index) => { 
+      if (!filterIndex.has(index)) {
+        res = res + letter
+      }
+    })
+  
+    return res
+  
+};
