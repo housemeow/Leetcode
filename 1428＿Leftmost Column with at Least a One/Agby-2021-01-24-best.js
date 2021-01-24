4bb06f1c032d7c1483279057cb72d26f28310fde
Agby <totoro255032@gmail.com>
2021-01-24 13:52:21 +0800

    Update Agby-2021-01-24-best.js

--- 1428＿Leftmost Column with at Least a One/Agby-2021-01-24-best.js
+++ 1428＿Leftmost Column with at Least a One/Agby-2021-01-24-best.js
@@ -1,5 +1,5 @@
 // Runtime: 68 ms, faster than 98.99% of JavaScript online submissions for Leftmost Column with at Least a One.
-// Memory Usage: 39.4 MB, less than 30.15% of JavaScript online submissions for Leftmost Column with at Least a One.
+// Memory Usage: 39.1 MB, less than 84.15% of JavaScript online submissions for Leftmost Column with at Least a One.
 
 /**
  * // This is the BinaryMatrix's API interface.
@@ -25,20 +25,20 @@
 var leftMostColumnWithOne = function(binaryMatrix) {
     const [rowNum, colNum] = binaryMatrix.dimensions()
     let res = -1
-    let start = colNum - 1
     
     for (let i=0; i<rowNum; i++) {
       if (binaryMatrix.get(i, colNum - 1) === 1) {
-        res=start
-        for(let j=start-1; j >= 0; j--) {
+        if (res === -1) res = colNum - 1 
+        
+        for(let j= res - 1; j >= 0; j--) {
            if (binaryMatrix.get(i, j) === 1) {
-             start = j
-             res = start
+             res = j
            } else {
              break
            }
         }
       }
+      if(res === 0) break
     }
   
     return res
