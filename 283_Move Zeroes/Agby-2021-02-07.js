acc95c84405a49ebc94104b0878ce96dae27782c
Agby <totoro255032@gmail.com>
2021-02-07 16:59:44 +0800

    Update Agby-2021-02-07.js

--- 283_Move Zeroes/Agby-2021-02-07.js
+++ 283_Move Zeroes/Agby-2021-02-07.js
@@ -2,14 +2,10 @@ Runtime: 76 ms, faster than 98.46% of JavaScript online submissions for Move Zer
 Memory Usage: 40.4 MB, less than 55.29% of JavaScript online submissions for Move Zeroes.
 
 var moveZeroes = function(nums) {
-  let zeroCount = 1
-    
-   for(let i = 0; i < nums.length - zeroCount; i++) {
+   for(let i = nums.length - 2; i >= 0; i--) {
       if (nums[i] === 0) {
         nums.splice(i, 1)
         nums.push(0)
-        i--
-        zeroCount++
       }
    }
 };
