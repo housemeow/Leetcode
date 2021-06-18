19fed55752cd5e4b9b67eb8330d0328da84432e0
Agby <totoro255032@gmail.com>
2021-06-18 16:18:12 +0800

    Update Agby-2021-01-20.js

--- 206_Reverse Linked List/Agby-2021-01-20.js
+++ 206_Reverse Linked List/Agby-2021-01-20.js
@@ -1,3 +1,20 @@
+// 2021/06/18
+var reverseList = function(head) {
+  
+  let last = null
+  
+  while (head) {
+    [head.next, last, head] = [last, head, head.next]
+  }
+  
+  
+  return last
+};
+
+
+
+// 2021/01/20
+
 Runtime: 72 ms, faster than 98.90% of JavaScript online submissions for Reverse Linked List.
 Memory Usage: 41 MB, less than 10.61% of JavaScript online submissions for Reverse Linked List.
 
