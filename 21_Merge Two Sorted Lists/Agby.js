58b5f23f5da4d5a8fa153bbb60e4805519907dce
Agby <totoro255032@gmail.com>
2021-06-18 16:19:42 +0800

    Update and rename Agby-2021-01-27.js to Agby.js

--- 21_Merge Two Sorted Lists/Agby.js
+++ 21_Merge Two Sorted Lists/Agby.js
@@ -1,18 +1,28 @@
+//2021-06-18
+var mergeTwoLists = function(l1, l2) {
+  const root = new ListNode(0, null)
+  let current = root
+  
+  while(l1 && l2) {
+    if (l1.val < l2.val) {
+      [current.next, l1] = [l1, l1.next]
+    } else {
+      [current.next, l2] = [l2, l2.next]
+    }
+    current = current.next
+  }
+  
+  current.next = l1 || l2
+  
+  return root.next
+};
+
+
+//2021-01-27
+
 Runtime: 84 ms, faster than 92.33% of JavaScript online submissions for Merge Two Sorted Lists.
 Memory Usage: 39.5 MB, less than 99.70% of JavaScript online submissions for Merge Two Sorted Lists.
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
 var mergeTwoLists = function(l1, l2) {
   const head = new ListNode()
   let current = head
