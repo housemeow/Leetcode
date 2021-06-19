1d6ac65001a3d2fcbd4712f57c5180534d4769a6
Agby <totoro255032@gmail.com>
2021-06-19 21:22:19 +0800

    Update Agby-2021-01-22.js

--- 70_Climbing Stairs/Agby-2021-01-22.js
+++ 70_Climbing Stairs/Agby-2021-01-22.js
@@ -1,3 +1,18 @@
+// 2021/06/19
+var climbStairs = function(n) {
+  const steps = [1, 1]
+  
+  for (let index = 2; index <= n; index++) {
+    steps[index] = steps[index - 1] + steps[index - 2] 
+  }
+  
+  return steps[n]
+};
+
+
+
+
+// 2021/01/22
 // Runtime: 68 ms, faster than 96.68% of JavaScript online submissions for Climbing Stairs.
 // Memory Usage: 38.3 MB, less than 79.79% of JavaScript online submissions for Climbing Stairs.
 
