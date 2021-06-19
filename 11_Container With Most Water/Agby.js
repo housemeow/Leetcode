2392f7314808344218c74e793d7bfacfdb8bbb2b
Agby <totoro255032@gmail.com>
2021-06-19 14:19:25 +0800

    Update and rename Agby-2021-1-1-best to Agby.js

--- 11_Container With Most Water/Agby.js
+++ 11_Container With Most Water/Agby.js
@@ -1,3 +1,44 @@
+// 2021/06/19
+
+var maxArea = function(height) {
+  let left = 0
+  let right = height.length - 1
+  let res = 0
+  
+  while (left < right) {
+    if (height[left] < height?.[left - 1]) {
+      left ++
+      continue
+    }
+    
+    if (height[right] < height?.[right + 1]) {
+      right --
+      continue
+    }
+    
+    const containerWidth = right - left
+    const containerHeight = height[right] > height[left] ? height[left] : height[right]
+    const sum = containerWidth * containerHeight
+    
+    res = res < sum ? sum : res
+    
+    if (height[left] > height[right]) {
+      right --
+      continue
+    }
+    
+    left++
+  }
+  
+  return res
+};
+
+
+
+
+
+//2021/1/1
+
 // 一開始只是想嘗試使用 disscuss 推薦的 two points pattern
 // 在做的時候忽然想到如果下一個比這一個更短 那根本連計算都可以跳過 所以才出現了第二層的 while
 
