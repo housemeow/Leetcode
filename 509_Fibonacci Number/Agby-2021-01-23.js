3b26c3301123bb810436c750e01827b5192cf0a6
Agby <totoro255032@gmail.com>
2021-06-19 13:37:51 +0800

    Update Agby-2021-01-23.js

--- 509_Fibonacci Number/Agby-2021-01-23.js
+++ 509_Fibonacci Number/Agby-2021-01-23.js
@@ -1,3 +1,23 @@
+//2021/06/19
+
+var fib = function(n) { 
+  if( n < 2 ) return n
+  
+  let last2 = 0
+  let last1 = 1
+  
+  for (let i = 2; i < n; i++) {
+    [last2, last1] = [last1, last1 + last2]
+  }
+  
+   return last2 + last1
+};
+
+
+
+
+// 2021/01/23
+
 Runtime: 60 ms, faster than 99.90% of JavaScript online submissions for Fibonacci Number.
 Memory Usage: 38.3 MB, less than 78.64% of JavaScript online submissions for Fibonacci Number.
 
