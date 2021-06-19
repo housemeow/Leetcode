1db323661df7ba581b92af0034d7f5ecd2791571
Agby <totoro255032@gmail.com>
2021-06-20 00:52:58 +0800

    Update Agby-2021-02-09.js

--- 104_Maximum Depth of Binary Tree/Agby-2021-02-09.js
+++ 104_Maximum Depth of Binary Tree/Agby-2021-02-09.js
@@ -1,3 +1,15 @@
+// 2021/06/20
+var maxDepth = function(root) { 
+  if (!root) return 0
+  
+  const left = maxDepth(root.left)
+  const right = maxDepth(root.right)
+  
+  return (left > right ? left : right) + 1 
+};
+
+
+// 2021/02/09
 Runtime: 80 ms, faster than 94.65% of JavaScript online submissions for Maximum Depth of Binary Tree.
 Memory Usage: 41.8 MB, less than 25.30% of JavaScript online submissions for Maximum Depth of Binary Tree.
 
