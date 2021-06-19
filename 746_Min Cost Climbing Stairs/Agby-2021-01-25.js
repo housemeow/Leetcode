bb90d9094dc71083a1a2f757e569270aae5e5cba
Agby <totoro255032@gmail.com>
2021-06-19 23:16:06 +0800

    Update Agby-2021-01-25.js

--- 746_Min Cost Climbing Stairs/Agby-2021-01-25.js
+++ 746_Min Cost Climbing Stairs/Agby-2021-01-25.js
@@ -1,3 +1,22 @@
+// 2021/06/19
+// 想很久
+var minCostClimbingStairs = function(cost) {
+  const start = cost[0] < cost[1] ? cost[0] : cost[1]
+  const pathCost = [0, start]
+  
+  for (let step = 2; step < cost.length; step++) {
+    const pathA = pathCost[step - 1] + cost[step]
+    const pathB = pathCost[step - 2] + cost[step - 1]
+    
+    pathCost[step] = pathA < pathB ? pathA : pathB
+  }
+
+  return pathCost[cost.length - 1]
+};
+
+
+// 2021/01/25
+
 // Runtime: 76 ms, faster than 97.65% of JavaScript online submissions for Min Cost Climbing Stairs.
 // Memory Usage: 39 MB, less than 78.49% of JavaScript online submissions for Min Cost Climbing Stairs.
 
