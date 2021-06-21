2df7d27551b15bd02e69e9cb8fb34f49aac0f94e
Agby <totoro255032@gmail.com>
2021-06-21 17:08:59 +0800

    Update Agby-2021-02-08.js

--- 226_Invert Binary Tree/Agby-2021-02-08.js
+++ 226_Invert Binary Tree/Agby-2021-02-08.js
@@ -1,5 +1,18 @@
-Runtime: 68 ms, faster than 98.56% of JavaScript online submissions for Invert Binary Tree.
-Memory Usage: 39 MB, less than 20.55% of JavaScript online submissions for Invert Binary Tree.
+// 2021/06/21
+
+function invertTree(root) {
+  if (!root) return root;
+  
+  [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
+  
+  return root
+};
+
+
+
+// 2021/02/08
+// Runtime: 68 ms, faster than 98.56% of JavaScript online submissions for Invert Binary Tree.
+// Memory Usage: 39 MB, less than 20.55% of JavaScript online submissions for Invert Binary Tree.
 
 var invertTree = function(root) {
   const dfsAndReverse = (node) => {
