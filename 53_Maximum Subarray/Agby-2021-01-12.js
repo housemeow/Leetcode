e9fabf5fa695054fe9cab9f07e1ab2dbcc2eedb3
Agby <totoro255032@gmail.com>
2021-06-21 16:13:29 +0800

    Update Agby-2021-01-12.js

--- 53_Maximum Subarray/Agby-2021-01-12.js
+++ 53_Maximum Subarray/Agby-2021-01-12.js
@@ -1,3 +1,26 @@
+// 2021/06/21
+
+var maxSubArray = function(nums) {
+  if (nums.length === 1) return nums[0]
+  
+  let res = nums[0]
+  let total = nums[0]
+  
+  for (let i = 1; i < nums.length; i++) {
+    const next = total + nums[i]
+    
+    total = next < nums[i] ? nums[i] : next
+    
+    if (res < total) {
+      res = total
+    }
+  }
+
+  return res
+};
+
+// 2021/01/12
+
 Runtime: 80 ms, faster than 89.26% of JavaScript online submissions for Maximum Subarray.
 Memory Usage: 39.3 MB, less than 46.88% of JavaScript online submissions for Maximum Subarray.
 
