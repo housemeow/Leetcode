edbf07310625a9f823c5040da18e1a39414fd98e
housemeow <housemeow@gmail.com>
2021-06-24 22:10:36 +0800

    Create Kellly.js

---
+++ 191_ Number of 1 Bits/Kellly.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let count = 0;
+    
+    while(n > 0) {
+        count += n & 1;
+        n >>>= 1;
+    }
+    
+    return count;
+};
