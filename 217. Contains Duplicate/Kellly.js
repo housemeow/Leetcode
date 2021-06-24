95ce9ba2ca8a6737ff2ae670c3198f65704a6674
housemeow <housemeow@gmail.com>
2021-06-24 15:08:22 +0800

    Create Kellly.js

---
+++ 217. Contains Duplicate/Kellly.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const set = new Set();
+    
+    for (const num of nums) {
+        if (set.has(num)) {
+            return true;
+        }
+        
+        set.add(num);
+    }
+    
+    return false;
+};
