9cc84f2b4ab82b13920c802e97bde5d3976e134b
housemeow <housemeow@gmail.com>
2021-06-24 12:10:13 +0800

    Create Kellly.js

---
+++ 226_Invert Binary Tree/Kellly.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+
+var invertTree = function(root) {
+    if (!root) return root;
+    
+    [root.left, root.right] = [root.right, root.left]
+    root.left = invertTree(root.left)
+    root.right = invertTree(root.right)
+    
+    return root;
+};
